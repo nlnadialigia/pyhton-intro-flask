60f7a38a290ff029b02ed114ff12e346798af724
Nadia <nlnadialigia@gmail.com>
2024-01-17 23:03:06 -0300

    create product

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
+
+db = SQLAlchemy(app)
+
+# Modelagem
+
+# Produto (id, name, price, description)
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(120), nullable=False)
+  price = db.Column(db.Float, nullable=False)
+  description = db.Column(db.Text, nullable=True)
 
 # Definir uma rota raiz e a função que será executada
 @app.route("/")
 def hello_world():
   return "Hello World"
+
+@app.route("/api/products/add", methods=["POST"])
+def add_product():
+  data = request.json
+  if "name" in data and "price" in data:
+    product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+    db.session.add(product)
+    db.session.commit()
+    return jsonify({"message": "Product added successfully"})
+  return jsonify({"message": "Invalid product data"}), 400
 
 # Debug não deverá ser utilizado em PROD
 if __name__ == "__main__":
